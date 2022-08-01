b44e288632fdfe3429f3cacdd96c9708952acfd7
Ted M. Young <tedyoung@gmail.com>
2022-08-01 15:35:17 -0700

    LAB #3: Fixed Primitive Obsession for Suit

--- src/main/java/com/jitterted/ebp/blackjack/Suit.java
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -1,32 +1,15 @@
 package com.jitterted.ebp.blackjack;
 
-import java.util.List;
-
 public enum Suit {
     SPADES("♠"),
     DIAMONDS("♦"),
     HEARTS("♥"),
     CLUBS("♣");
 
-    static final List<String> SUITS = List.of(SPADES.symbol(),
-                                              DIAMONDS.symbol(),
-                                              HEARTS.symbol(),
-                                              CLUBS.symbol());
     private final String symbol;
 
     Suit(String symbol) {
         this.symbol = symbol;
-    }
-
-    // SCAFFOLDING
-    @Deprecated
-    public static Suit from(String symbol) {
-        for (Suit suit : Suit.values()) {
-            if (symbol.equals(suit.symbol())) {
-                return suit;
-            }
-        }
-        return null;
     }
 
     public String symbol() {
