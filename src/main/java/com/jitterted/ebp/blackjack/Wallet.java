cd187d92602bd9bc2e7f5d29c0f0c62dba1b1d0c
Ted M. Young <tedyoung@gmail.com>
2022-08-02 15:35:31 -0700

    LAB #8: Refactor: extracted guard clause

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -17,16 +17,20 @@ public class Wallet {
         return balance;
     }
 
+    public void bet(int betAmount) {
+        requireSufficientBalanceToBet(betAmount);
+        balance -= betAmount;
+    }
+
+    private void requireSufficientBalanceToBet(int betAmount) {
+        if (betAmount > balance) {
+            throw new IllegalStateException();
+        }
+    }
+
     private void requireGreaterThanZeroAmount(int amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException();
         }
-    }
-
-    public void bet(int betAmount) {
-        if (betAmount > balance) {
-            throw new IllegalStateException();
-        }
-        balance -= betAmount;
     }
 }
