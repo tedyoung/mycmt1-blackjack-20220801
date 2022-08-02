dc92fdc4859fe770ff53d896af951dae0ea6f4dd
Ted M. Young <tedyoung@gmail.com>
2022-08-02 15:23:54 -0700

    LAB #8: Refactored test code to extract setup helper method

--- src/test/java/com/jitterted/ebp/blackjack/WalletBettingTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletBettingTest.java
@@ -8,19 +8,17 @@ public class WalletBettingTest {
     
     @Test
     public void walletWithBalance12WhenBet8BalanceIs4() throws Exception {
-        Wallet wallet = new Wallet();
-        wallet.addMoney(12);
+        Wallet wallet = createWalletWithInitialAmountOf(12);
 
         wallet.bet(8);
 
         assertThat(wallet.balance())
                 .isEqualTo(12 - 8);
     }
-    
+
     @Test
     public void walletWithBalance27WhenBet7And9BalanceIs11() throws Exception {
-        Wallet wallet = new Wallet();
-        wallet.addMoney(27);
+        Wallet wallet = createWalletWithInitialAmountOf(27);
 
         wallet.bet(7);
         wallet.bet(9);
@@ -30,7 +28,13 @@ public class WalletBettingTest {
     }
 
 
-    
+
+    private Wallet createWalletWithInitialAmountOf(int initialAmount) {
+        Wallet wallet = new Wallet();
+        wallet.addMoney(initialAmount);
+        return wallet;
+    }
+
 }
 
 
